53bab2287ccfabb0c5bd813c8c9d523b97bf5f22
Abhi70815 <abhishekchaurasia45621@gmail.com>
2024-06-17 22:00:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //your JS code here. If required.
 document.querySelector("#enterBtn").addEventListener('click', ()=>{
-	document.querySelector("#status").innerText="<h1>Entered Metaverse</h1>"
+	document.querySelector("#status").innerHTML="<h1>Entered Metaverse</h1>"
 })
